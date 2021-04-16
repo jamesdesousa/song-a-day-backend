425fdf9f6e66f2bbb4de74da900f5641618d141b
James <jamesdesousa45@gmail.com>
2021-04-16 14:10:17 -0400

    commit before changing post model

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,26 @@
 class LikesController < ApplicationController
+    def index
+        @likes = Like.order(:id)
+        render json: @likes
+    end
+
+    def new
+        @like = Like.new
+        render json: @like
+    end
+
+    def create 
+        @like = Like.create(like_params)
+        render json: @like
+    end
+
+    def destroy
+        @like = Like.find(params[:id])
+        @like.destroy
+    end
+
+    private
+    def like_params
+        params.permit(:post_id, :user_id)
+    end
 end
